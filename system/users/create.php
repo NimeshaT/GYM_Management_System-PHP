d786c588243427c8dd7ba3dc4bc43d9f00308595
NimeshaT <nimeshaijse2020@gmail.com>
2025-02-17 02:54:28 +0530

    file handling

--- system/users/create.php
+++ system/users/create.php
@@ -58,26 +58,71 @@ include '../nav.php';
                                 $message['lastName'] = "LastName should not be empty..!";
                             }
                             //validation end
-                            //Insert Record
+                            
+                            //profile image uploading
+                            if(empty($message)){
+                                $target_dir = "../uploads/";
+                                $target_file = $target_dir . basename($_FILES["profilePhoto"]["name"]);
+                                $uploadOk = 1;
+                                //path info extension->get extension of file type
+                                $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                                //check whether it is an image or not
+                                $check = getimagesize($_FILES["profilePhoto"]["tmp_name"]);
+                                if ($check !== false) {
+                                //Multi-purpose Internet Mail Extensions                       
+                                    $uploadOk = 1;
+                                } else {
+                                    $message['profilePhoto'] = "File is not an image.";
+                                    $uploadOk = 0;
+                                }
+                                
+                                // Check if file already exists->only check the content not image content
+                                if (file_exists($target_file)) {
+                                    $message['profilePhoto'] = "Sorry, file already exists.";
+                                    $uploadOk = 0;
+                                }
+                                
+                                // Check file size(KB)
+                                if ($_FILES["profilePhoto"]["size"] > 5000000) {
+                                    $message['profilePhoto'] = "Sorry, your file is too large.";
+                                    $uploadOk = 0;
+                                }
+                                
+                                // Allow certain file formats
+                                if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+                                    $message['profilePhoto'] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                                    $uploadOk = 0;
+                                }
+                                
+                                if ($uploadOk == 1) {
+                                    if (move_uploaded_file($_FILES["profilePhoto"]["tmp_name"], $target_file)) {
+                                        $Photo = htmlspecialchars(basename($_FILES["profilePhoto"]["name"]));
+                                    } else {
+                                        $message['profilePhoto'] = "Sorry, there was an error uploading your file.";
+                                    }
+                                }
+                            }
+                            
+                            //Insert Records
                             if (empty($message)) {
                                 $db = dbConn();
                                 $sql = "INSERT INTO tbl_instructors("
                                         . "userName,"
                                         . "password,"
-                                        . "title,"
+                                        . "titleId,"
                                         . "firstName,"
                                         . "LastName,"
                                         . "address,"
                                         . "email,"
-                                        . "telNo)VALUES("
+                                        . "telNo,profilePhoto)VALUES("
                                         . "'$userName',"
                                         . "'" . sha1($password) . "',"
-                                        . "'$title',"
+                                        . "'$titleId',"
                                         . "'$firstName',"
                                         . "'$lastName',"
                                         . "'$address',"
                                         . "'$email',"
-                                        . "'$telNo')";
+                                        . "'$telNo','$Photo')";
 
                                 $db->query($sql);
                             }
@@ -88,7 +133,65 @@ include '../nav.php';
                             $btn = 'primary';
                         }
 
-//                        ---------------------Edit account-----------------------------
+                        //Update Records
+                        if ($_SERVER['REQUEST_METHOD'] == 'POST' && @$action == 'update_account') {
+                            
+                            if (empty($message) AND!empty($_FILES["profilePhoto"]["name"])) {
+                                $target_dir = "../uploads/";
+                                $target_file = $target_dir . basename($_FILES["profilePhoto"]["name"]);
+                                $uploadOk = 1;
+                                $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                                $check = getimagesize($_FILES["profilePhoto"]["tmp_name"]);
+                                if ($check !== false) {
+                                //Multi-purpose Internet Mail Extensions                       
+                                    $uploadOk = 1;
+                                } else {
+                                    $message['profilePhoto'] = "File is not an image.";
+                                    $uploadOk = 0;
+                                }
+                                // Check if file already exists
+                                if (file_exists($target_file)) {
+                                    $message['profilePhoto'] = "Sorry, file already exists.";
+                                    $uploadOk = 0;
+                                }
+                                // Check file size
+                                if ($_FILES["profilePhoto"]["size"] > 5000000) {
+                                    $message['profilePhoto'] = "Sorry, your file is too large.";
+                                    $uploadOk = 0;
+                                }
+
+                                // Allow certain file formats
+                                if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+                                    $message['profilePhoto'] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                                    $uploadOk = 0;
+                                }
+                                if ($uploadOk == 1) {
+                                    if (move_uploaded_file($_FILES["profilePhoto"]["tmp_name"], $target_file)) {
+                                        $Photo = htmlspecialchars(basename($_FILES["profilePhoto"]["name"]));
+                                    } else {
+                                        $message['profilePhoto'] = "Sorry, there was an error uploading your file.";
+                                    }
+                                }
+                            } else {
+                                $Photo = $PreviousProfilePhoto;
+                            }
+
+                            $db = dbConn();
+                            $sql = "UPDATE tbl_instructors SET "
+                                    . "titleId='$titleId',"
+                                    . "firstName='$firstName',"
+                                    . "lastName='$lastName',"
+                                    . "address='$address',"
+                                    . "email='$email',"
+                                    . "telNo='$telNo',"
+                                    . "profilePhoto='$Photo' "
+                                    . "WHERE instructorId='$instructorId'";
+                            $db->query($sql);
+                            $submit = 'Update';
+                            $btn = 'success';
+                        }
+                        
+                        //Edit Records
                         if ($_SERVER['REQUEST_METHOD'] == 'POST' && @$action == 'edit_account') {
 
                             //echo 'edit';
@@ -99,12 +202,13 @@ include '../nav.php';
 
                             $row = $result->fetch_assoc();
 
-                            $title = $row['title'];
+                            $titleId = $row['titleId'];
                             $firstName = $row['firstName'];
                             $lastName = $row['lastName'];
                             $address = $row['address'];
                             $email = $row['email'];
                             $telNo = $row['telNo'];
+                            $profilePhoto = $row['profilePhoto'];
                             $userName = $row['userName'];
                             $password = $row['password'];
                             $instructorId = $row['instructorId'];
@@ -115,39 +219,33 @@ include '../nav.php';
                             $btn = 'success';
                         }
 
-                        //                        ---------------------Update account-----------------------------
-                        if ($_SERVER['REQUEST_METHOD'] == 'POST' && @$action == 'update_account') {
-
-                            $db = dbConn();
-                            $sql = "UPDATE tbl_instructors SET "
-                                    . "title='$title',"
-                                    . "firstName='$firstName',"
-                                    . "lastName='$lastName',"
-                                    . "address='$address',"
-                                    . "email='$email',"
-                                    . "telNo='$telNo' "
-                                    . "WHERE instructorId='$instructorId'";
-                            $db->query($sql);
-                            $submit = 'Update';
-                            $btn = 'success';
-                        }
-
-//                        ---------------------Delete account-----------------------------
+                        //Start Delete
                         ?>
                         <div class="card-header">
                             <h3 class="card-title"><?php echo @$form_title; ?> User Account</h3>
                         </div>
-                        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
                             <div class="card-body">
                                 <div class="form-group">
                                     <?php
-//$title='';
+                                    //$title='';
+                                    $db= dbConn();
+                                    $sql="SELECT * FROM tbl_instructor_title";
+                                    $result=$db->query($sql);
                                     ?>
                                     <label for="title">Select Title</label>
-                                    <select class="form-control" name="title" id="title">
+                                    <select class="form-control" name="titleId" id="titleId">
                                         <option value="">--</option>
-                                        <option value="Mr." <?php if (@$title == 'Mr.') { ?> selected <?php } ?>>Mr.</option>
-                                        <option value="Miss."<?php if (@$title == 'Miss.') { ?> selected <?php } ?>>Miss.</option>
+                                        <?php
+                                        //fetch assoc convert data associative array
+                                        if($result->num_rows>0){
+                                            while ($row=$result->fetch_assoc()){
+                                        ?>
+                                        <option value="<?php echo $row['titleId']; ?>" <?php if (@$titleId == $row['titleId']) { ?> selected <?php } ?>><?php echo $row['titleName']; ?></option>
+                                        <?php
+                                        }
+                                        }
+                                        ?>
                                     </select>
                                 </div>
                                 <div class="form-group">
@@ -182,14 +280,11 @@ include '../nav.php';
                                     <label for="password">Password</label>
                                     <input type="password" class="form-control" id="password" name="password" placeholder="Password" value="<?php echo @$password ?>">
                                 </div>
-                                <div class="form-group">
-                                    <label for="exampleInputFile">File input</label>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input type="file" class="custom-file-input" id="exampleInputFile">
-                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                        </div>
-                                    </div>
+                                <div class="mb-3">
+                                    <label for="profilePhoto">Profile Image</label>
+                                    <input type="file" class="form-control" id="profilePhoto" name="profilePhoto">
+                                    <input type="hidden" name="PreviousProfilePhoto" value="<?php echo @$profilePhoto; ?>">
+                                    <div class="text-danger"><?php echo @$message['profilePhoto']; ?></div>
                                 </div>
                             </div>
                             <!-- /.card-body -->
@@ -214,29 +309,28 @@ include '../nav.php';
 //                            
 //                            $submit='Save';
                         ?>
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Delete Confirmation</h3>
-                        </div>
-                        <div class="card-body">
-                            
-                            Are you sure want to delete this record?
-                            <br><br> 
-                        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-                            <input type="hidden" name="instructorId" value="<?php echo $instructorId; ?>">
-                            <button type="submit" class="btn btn-warning" name="action" value="delete_account_confirm">Yes</button>
-                            <button type="submit" class="btn btn-danger" name="action" value="delete_account_cancel">No</button>
-                        </form>
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Delete Confirmation</h3>
                             </div>
-                    </div>
+                            <div class="card-body">
+
+                                Are you sure want to delete this record?
+                                <br><br> 
+                                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                                    <input type="hidden" name="instructorId" value="<?php echo $instructorId; ?>">
+                                    <button type="submit" class="btn btn-warning" name="action" value="delete_account_confirm">Yes</button>
+                                    <button type="submit" class="btn btn-danger" name="action" value="delete_account_cancel">No</button>
+                                </form>
+                            </div>
+                        </div>
                         <?php
                     }
                     if ($_SERVER['REQUEST_METHOD'] == 'POST' && @$action == 'delete_account_confirm') {
-                            
-                            $db= dbConn();
-                            $sql="DELETE FROM tbl_instructors WHERE instructorId='$instructorId'";
-                            $db->query($sql);
-                            
+
+                        $db = dbConn();
+                        $sql = "DELETE FROM tbl_instructors WHERE instructorId='$instructorId'";
+                        $db->query($sql);
                     }
                     ?>
                     <div class="card">
@@ -244,9 +338,23 @@ include '../nav.php';
                             <h3 class="card-title">Table of User Details</h3>
                         </div>
                         <div class="card-body">
+<!--                            -----------------Search BAR----------------------->
+                            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                                <input type="text" name="firstName" id="firstName" class="form-control" placeholder="Enter First Name">
+                                <button type="submit" class="btn btn-success mt-2 mb-2" name="action" value="search_account">Search</button>
+                            </form>
+
                             <?php
+//                            ----------------Search Account-search bar------------------
+                            $where=null;
+                            if ($_SERVER['REQUEST_METHOD'] == 'POST' && @$action == 'search_account') {
+                                if(!empty($firstName)){
+                                    $where.="WHERE firstName='$firstName'";
+                                }
+                            }
+                            
                             $db = dbConn();
-                            $sql = "SELECT * FROM tbl_instructors";
+                            $sql = "SELECT * FROM tbl_instructors $where";
                             $result = $db->query($sql);
                             ?>
                             <table id="user_list" class="table table-bordered table-hover">
@@ -269,7 +377,7 @@ include '../nav.php';
 
                                                 <td><?php echo $row['firstName']; ?> <?php echo $row['lastName']; ?></td>
                                                 <td><?php echo $row['email']; ?></td>
-                                                <td><?php echo $row['profilePhoto']; ?></td>
+                                                <td><img class="img-fluid" width="100" src="<?php echo SITE_URL; ?>uploads/<?php echo $row['profilePhoto']; ?>"></td>
                                                 <td>
                                                     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
                                                         <input type="hidden" name="instructorId" value="<?php echo $row['instructorId']; ?>">
